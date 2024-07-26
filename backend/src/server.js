852cecbdbbc6d771d0011a521cf7eb8c9bf8aa0f
Apparao <chowdaryapparao1@gmail.com>
2024-07-26 23:40:04 +0530

    bugs fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db'); 
 const fs = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
